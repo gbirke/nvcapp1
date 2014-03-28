7d5c9a6090a9bc28050f64cf50cd28057044fa25
Gabriel Birke <gb@birke-software.de>
2014-03-28 15:25:29 +0100

    Update to Symfony 2.4
    Remove require.js bundle
    Remove old bundles that are no longer supplied with Symfony
    Remove config lines that relate to removed bundles.
    Add shell script to set folder permissions on Mac OS X

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,14 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
 
             // Vendor Bundles
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new Hearsay\RequireJSBundle\HearsayRequireJSBundle(),
 
             //My modules
             new Birke\NvcBundle\BirkeNvcBundle(),
